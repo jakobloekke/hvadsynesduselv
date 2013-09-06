250bb93685700438ce849292a904f84238ed72af
jakobloekke <jakob@jakobloekkemadsen.com>
2013-09-06 14:03:35 +0200

    Updates

---
+++ README.md
@@ -0,0 +1,2 @@
+hvadsynesduselv
+===============
---
+++ js/app.js
@@ -0,0 +1,34 @@
+var app = angular.module('app', []);
+
+app.constant('SETTINGS', {
+    'views_url': '/views'
+});
+
+app.controller('MainCtrl', function ($scope, SETTINGS) {
+    $scope.farve = 'green';
+});
+
+app.filter('reverse', function() {
+    return function(items) {
+        return items.slice().reverse();
+    };
+});
+
+app.config(function ($routeProvider, $locationProvider, SETTINGS) {
+    // Uncomment to simulate non-html5 browser:
+    /*$provide.decorator('$sniffer', function($delegate) {
+     $delegate.history = false;
+     return $delegate;
+     });
+     */
+
+    $locationProvider
+        .html5Mode(true);
+
+    $routeProvider
+        .when('/', {
+            templateUrl: SETTINGS.views_url + "/frontpage.html",
+            controller: 'MainCtrl'
+        })
+        .otherwise({ redirectTo: '/' });
+});
